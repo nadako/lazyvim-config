a6e88855b69ead5c7e020e748b28c93526bae612
Dan Korostelev <nadako@gmail.com>
2025-03-25 15:30:15 +0100

    setup hlsl comment string

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,13 @@ vim.opt.exrc = true
 vim.opt.undofile = true
 vim.opt.updatetime = 250
 vim.g.autoformat = false
+
 vim.filetype.add({
   extension = {
-    hlsl = "hlsl",
+    hlsl = function(path, bufnr, ext)
+      vim.bo[bufnr].commentstring = "//%s"
+      return "hlsl"
+    end,
   },
   pattern = {
     [".*%.glsl%..*"] = "glsl",

f543f776e6003c3f7033f93bee08abd300dff493
Dan Korostelev <nadako@gmail.com>
2025-03-25 14:43:53 +0100

    add overseer, it's actually nice

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<leader>cb", "<cmd>make<CR>", { desc = "Build code" })
+vim.api.nvim_create_user_command("OverseerRestartLast", function()
+  local overseer = require("overseer")
+  local tasks = overseer.list_tasks({ recent_first = true })
+  if vim.tbl_isempty(tasks) then
+    vim.notify("No tasks found", vim.log.levels.WARN)
+  else
+    overseer.run_action(tasks[1], "restart")
+  end
+end, {})
+vim.keymap.set("n", "<leader>ol", "<cmd>OverseerRestartLast<CR>", { desc = "Restart Last Task" })

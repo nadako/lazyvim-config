bd68349f93d517604a1d701a3abf60c5e140c71c
Dan Korostelev <nadako@gmail.com>
2025-03-25 14:55:18 +0100

    better setup shader filetypes

--- lua/config/autocmds.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = vim.api.nvim_create_augroup("SetGLSLFileType", { clear = true }),
-  pattern = { "*.glsl.frag", "*.glsl.vert" },
-  callback = function()
-    vim.opt_local.filetype = "glsl"
-  end,
-})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,11 @@ vim.opt.exrc = true
 vim.opt.undofile = true
 vim.opt.updatetime = 250
 vim.g.autoformat = false
+vim.filetype.add({
+  extension = {
+    hlsl = "hlsl",
+  },
+  pattern = {
+    [".*%.glsl%..*"] = "glsl",
+  },
+})
